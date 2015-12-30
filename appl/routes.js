d575975688a85f0d250135d8f72fcf30f8de6374
giles lauwers <gileslauwers@hotmail.be>
2015-12-30 14:59:55 +0100

    routes nekeer proper gezet

--- appl/routes.js
+++ appl/routes.js
@@ -5,41 +5,24 @@
 var ticketController = require('./controllers/serverTicketController.js');
 var userController = require('./controllers/serverUserController.js');
 
+
 module.exports = function(app, passport) {
 
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+// alle routes voor navigatie ************************************************************
+
+
     app.get('/', function(req, res) {
         res.sendfile('./public/index.html');
     });
 
-
-
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
     app.get('/login', function(req, res) {
 
-        // render the page and pass in any flash data if it exists
-        //res.render('login.ejs');
         res.sendfile('./public/login.html');
     });
 
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
-
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
     app.get('/signup', function(req, res) {
-        // render the page and pass in any flash data if it exists
-       // res.render('signup.ejs', { message: req.flash('signupMessage') });
         res.sendfile('./public/register.html');
     });
-
 
     app.get('/werking', isLoggedIn ,function(req, res) {
 
@@ -53,61 +36,30 @@ module.exports = function(app, passport) {
     app.get('/overzichttickets',function(req,res){
         res.sendfile('./public/overzichttickets.html');
     });
+
     app.get('/mytickets',function(req,res){
         res.sendfile('./public/mytickets.html');
     });
 
-    // process the signup form
-    // app.post('/signup', do all our passport stuff here);
-
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-      //  res.render('profile.ejs', {
-       //     user : req.user // get the user out of session and pass to template
-       // });
         res.json(req.user);
-       // res.redirect('/');
-
     });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
-    });
-
-
-
-    // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : 'index.html', // redirect to the secure profile section
-        failureRedirect : 'register.html', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/', // redirect to the secure profile section
-        failureRedirect : 'login.html', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-    app.get('/api/getuser', function(req,res){
-        res.json(req.user);
-       // return userController.getUser(req,res);
     });
 
     app.get('/ticketregistreren',function(req,res){
         return ticketController.getTickets(req,res);
     });
 
-    app.post('/ticketregistreren',function(req,res){
-        return ticketController.create(req,res);
+
+    // alle api calls ********************************************************************
+
+    app.get('/api/getuser', function(req,res){
+        res.json(req.user);
+        // return userController.getUser(req,res);
     });
 
     app.get('/api/tickets',function(req,res){
@@ -122,17 +74,33 @@ module.exports = function(app, passport) {
         return ticketController.getMyTickets(req,res);
     });
 
+    // alle posts in de nav *******************************************************************
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : 'index.html', // redirect to the secure profile section
+        failureRedirect : 'register.html', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/', // redirect to the secure profile section
+        failureRedirect : 'login.html', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    app.post('/ticketregistreren',function(req,res){
+        return ticketController.create(req,res);
+    });
 
 
 };
 
-// route middleware to make sure a user is logged in
+// kijken of de gebruiker ingelogd is *****************************************************
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
 
-    // if they aren't redirect them to the home page
+    // niet ingelogd, naar loginpagina
     res.redirect('/login');
 }
